1c8992f26e283267262058885b66a3fac27e4a81
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2023-09-11 20:48:38 -0400

    Comitting to work on player pool

--- Assets/Scripts/Testing/ScoreBoardTriggerTest.cs
+++ Assets/Scripts/Testing/ScoreBoardTriggerTest.cs
@@ -1,8 +1,12 @@
 ﻿
+using System.Net;
+using System.Runtime.Remoting.Messaging;
 using TMPro;
 using UdonSharp;
 using UnityEngine;
 using UnityEngine.UI;
+using VRC.SDK3.Components;
+using VRC.SDK3.Data;
 using VRC.SDKBase;
 
 namespace Testing
@@ -10,19 +14,23 @@ namespace Testing
     public class ScoreBoardTriggerTest : UdonSharpBehaviour
     {
         // Unity objects
-        [SerializeField] private Transform[] rows;
-
+        [SerializeField] private VRCObjectPool rows;
+        
         private int _numberOfPlayers = 0;
-        private VRCPlayerApi[] _players = new VRCPlayerApi[40];
+        private bool _isJoined = false;
+        private DataDictionary _players = new DataDictionary();
+        private VRCPlayerApi localPlayer;
+
+        // TMP_
 
         void Start()
         {
+            localPlayer = Networking.LocalPlayer;
         }
 
         public override void OnPlayerJoined(VRCPlayerApi player)
         {
             _numberOfPlayers++;
-            _players[_numberOfPlayers - 1] = player;
         }
 
         public override void OnPlayerLeft(VRCPlayerApi player)
@@ -32,10 +40,33 @@ namespace Testing
 
         public override void OnPlayerTriggerEnter(VRCPlayerApi player)
         {
-            Debug.Log(rows[0]);
-            rows[0].GetChild(0).GetChild(2).GetComponent<Text>().text = "1";
-            rows[0].GetChild(1).GetChild(2).GetComponent<Text>().text = player.displayName;
-            rows[0].GetChild(2).GetChild(2).GetComponent<Text>().text = "0";
+            if (player == localPlayer)
+            {
+                if (_isJoined) return;
+
+                for (var i = 0; i < rows.Pool.Length; i++)
+                {
+                    if (rows.Pool[i].activeSelf) continue;
+                                                    
+                    rows.Pool[i].SetActive(true);
+                
+                    var rank = rows.Pool[i].transform.GetChild(0).GetChild(2).GetComponent<TextMeshProUGUI>();
+                    var displayName = rows.Pool[i].transform.GetChild(1).GetChild(2).GetComponent<TextMeshProUGUI>();
+                    var score = rows.Pool[i].transform.GetChild(2).GetChild(2).GetComponent<TextMeshProUGUI>();
+
+                    rank.text = i.ToString();
+                    displayName.text = localPlayer.displayName;
+                    score.text = "0";
+                
+                    _isJoined = true;
+                                
+                    return;
+                }
+            }
+            else
+            {
+                
+            }
         }
     }
 }
