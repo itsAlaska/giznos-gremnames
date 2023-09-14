538227316349561664e2744b549b3abf5d26b4ba
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2023-09-13 22:24:11 -0400

    forced to commit

---
+++ Assets/Scripts/Cards/Card.cs
@@ -0,0 +1,41 @@
+﻿using TMPro;
+using UdonSharp;
+using UnityEngine;
+
+namespace Cards
+{
+    public class Card : UdonSharpBehaviour
+    {
+        public string[] words;
+
+        // [UdonSynced, FieldChangeCallback(nameof(Word))] public string word;
+        public string word;
+        
+        [SerializeField] private TextMeshProUGUI wordField;
+
+        public void PickWord()
+        {
+            int wordListIndex = Random.Range(0, words.Length + 1);
+            
+            Debug.Log($"The wordListIndex value is {wordListIndex}");
+            word = words[wordListIndex];
+            wordField.text = word;
+
+        }
+
+        // public void UpdateCardDisplay(string currentWord)
+        // {
+        //     wordField.text = currentWord;
+        // }
+
+        // public string Word
+        // {
+        //     set
+        //     {
+        //         word = value;
+        //         UpdateCardDisplay(word);
+        //     }
+        //     get => word;
+        // }
+    }
+}
---
+++ Assets/Scripts/Testing/ToggleBox.cs
@@ -0,0 +1,26 @@
+﻿using Cards;
+using UdonSharp;
+using UnityEngine;
+
+namespace Testing
+{
+    
+    public class ToggleBox : UdonSharpBehaviour
+    {
+        // [SerializeField] private WordList wordList;
+        [SerializeField] private Card card;
+        
+        private void Interact()
+        {
+            if (card.words.Length > 0)
+            {
+                card.PickWord();
+            }
+            else
+            {
+                // wordList._DownloadList();
+            }
+            
+        }
+    }
+}
