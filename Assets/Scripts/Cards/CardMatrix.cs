8e3f6a14886de2ed2707f3806e4a3b159f31718e
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2023-09-15 00:59:02 -0400

    Adds outlines to cards for clue giver

--- Assets/Scripts/Cards/CardMatrix.cs
+++ Assets/Scripts/Cards/CardMatrix.cs
@@ -2,14 +2,15 @@
 using UdonSharp;
 using UnityEngine;
 using VRC.SDK3.Data;
+using VRC.SDKBase;
+using VRC.Udon.Common.Interfaces;
 
 namespace Cards
 {
     public class CardMatrix : UdonSharpBehaviour
     {
-        // [SerializeField] private GameObject[] cards;
+        [SerializeField] private GameObject[] outlines = new GameObject[16];
         [SerializeField] private TextMeshProUGUI[] wordFields = new TextMeshProUGUI[16];
-
         
         public string[] allWords;
 
@@ -18,11 +19,43 @@ namespace Cards
 
         public override void OnDeserialization()
         {
+            ResetOutlines();
             UpdateCards();
         }
 
         public void PopulateWordList()
         {
+            ResetOutlines();
+            
+            if (Networking.IsOwner(Networking.LocalPlayer, gameObject))
+            {
+                var outlineIndices = new int[6];
+                for (var i = 0; i < 6; i++)
+                {
+                    int outlineIndex = Random.Range(0, 16);
+
+                    if (i > 0)
+                    {
+                        for (var j = 0; j < i; j++)
+                        {
+                            if (outlineIndices[j] == outlineIndex)
+                            {
+                                while (outlineIndices[j] == outlineIndex)
+                                {
+                                    outlineIndex = Random.Range(0, 16);
+                                }
+                            }
+                        }
+                    }
+
+                    outlineIndices[i] = outlineIndex;
+                }
+                for (int g = 0; g < 6; g++)
+                {
+                    outlines[outlineIndices[g]].SetActive(true);
+                }
+            }
+            
             var wordIndices = new int[16];
             var wordsArrayForThisRound = new string[16];
             
@@ -54,6 +87,11 @@ namespace Cards
 
             ThisRoundWords = wordsArrayForThisRound;
             RequestSerialization();
+        }
+
+        public void ResetOutlines()
+        {
+            foreach (var outline in outlines) outline.SetActive(false);
         }
 
         public void UpdateCards()
